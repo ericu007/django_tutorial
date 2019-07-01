9afc2c03c524f2cb41708b0d03af5a107c4060e3
ericu007 <ericu007@gmail.com>
2019-07-01 14:14:33 +0900

    complete

---
+++ description.py
@@ -0,0 +1,14 @@
+"""
+장고로 웹 사이트 만들기
+1. 파이참 프로젝트 만들기
+2. 장고 설치 : pip install django
+3. 장고 프로젝트 만들기 :  django-admin startproject config .
+config : 설정 파일 폴더이름
+. : 현재 폴더에 파일과 폴더를 구성하겠다.
+
+4. 데이터베이스 초기화 : python manage.py migrate
+
+5. 앱 만들기 : python manage.py startapp [AppName]
+6. MTV : Model - View - Template - URL
+
+"""
